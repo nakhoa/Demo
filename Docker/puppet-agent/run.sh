0d59bb552a26eea01fd73160952f1b9aa0728122
Nguyen Anh Khoa <anhkhoa301084@gmail.com>
2017-05-24 23:28:01 +0700

    generate certname for each container

--- Docker/puppet-agent/run.sh
+++ Docker/puppet-agent/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-
 apt-get update
+export mycertname="`cat /etc/puppetlabs/puppet/puppet.conf | grep "certname"`"
+if [ -z "$mycertname" ]; then
+	echo "certname = `uuidgen -r`.`dnsdomainname`" >> /etc/puppetlabs/puppet/puppet.conf
+fi
 /opt/puppetlabs/bin/puppet agent -t
 tail -f /dev/null
